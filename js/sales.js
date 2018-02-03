e467c46b975f98f5fc707c9a7cf0ba11ac2df1d0
Khoa Huynh <khoawin92@gmail.com>
2018-02-03 15:40:04 -0800

    added total totals to work with user input

--- js/sales.js
+++ js/sales.js
@@ -3,6 +3,7 @@
 //making list for hours
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am','12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 var hourlyTotalsArray = [];
+var allLocationsSales = [];
 
 for (var i = 0; i < hours.length; i++) {
   var trEl = document.createElement('tr');
@@ -21,7 +22,7 @@ for (var i = 0; i < hours.length; i++) {
 var totalEl = document.createElement('th');
 var totalText = document.createTextNode('Total');
 totalEl.appendChild(totalText);
-var tfooterEl = document.getElementById('total');
+var tfooterEl = document.getElementById('totalRow');
 tfooterEl.appendChild(totalEl);
 
 //constructor function
@@ -66,55 +67,55 @@ function Stand(idTag, name, minCust, maxCust, cookiesAvg) {
     var totalEl = document.createElement('td');
     var totalText = document.createTextNode(location.total);
     totalEl.appendChild(totalText);
-    var tfooterEl = document.getElementById('total');
+    var tfooterEl = document.getElementById('totalRow');
     tfooterEl.appendChild(totalEl);
+  }
+  makeTable(this);
+  allLocationsSales.push(this.hourlySales);
+}
 
+new Stand('pike','first & pike', 23, 65, 6.3);
+new Stand('seaTac', 'SeaTac Airport', 3, 24, 1.2);
+new Stand('seattleCenter', 'Seattle Center', 11, 38, 3.7);
+new Stand('capitolHill', 'Capitol Hill', 20, 38, 2.3);
+new Stand('alki', 'Alki', 2, 16, 4.6);
+
+function makeTotal(){
+  //create totals for the hours
+  var tableHead = document.getElementById('tableHead');
+  var hoursTotal = document.createElement('th');
+  hoursTotal.textContent = 'Total';
+  hoursTotal.className = 'total';
+  tableHead.appendChild(hoursTotal);
+
+  for (var k = 0; k < hours.length; k++) {
+    hourlyTotalsArray[k] = 0;
+    for (var m = 0; m < allLocationsSales.length; m++) {
+      hourlyTotalsArray[k] += allLocationsSales[m][k];
+    }
   }
 
-  makeTable(this);
+  for (var l = 0; l < hours.length; l++) {
+    var trTotalEl = document.getElementById(hours[l]);
+    var totalTd = document.createElement('td');
+    totalTd.className = 'total';
+    totalTd.textContent = hourlyTotalsArray[l];
+    trTotalEl.appendChild(totalTd);
+  }
+
+  //creating totaltotal
+  var totalTotal = 0;
+  for (var o = 0; o < hourlyTotalsArray.length; o++) {
+    totalTotal += hourlyTotalsArray[o];
+  }
+
+  var totalTotalEl = document.createElement('td');
+  totalTotalEl.textContent = totalTotal;
+  totalTotalEl.className = 'total';
+  totalTotalEl.setAttribute('id', 'totalTotal');
+  tfooterEl.appendChild(totalTotalEl);
 }
-
-
-var pike = new Stand('pike','first & pike', 23, 65, 6.3);
-var seaTac = new Stand('seaTac', 'SeaTac Airport', 3, 24, 1.2);
-var seattleCenter = new Stand('seattleCenter', 'Seattle Center', 11, 38, 3.7);
-var capitolHill = new Stand('capitolHill', 'Capitol Hill', 20, 38, 2.3);
-var alki = new Stand('alki', 'Alki', 2, 16, 4.6);
-
-
-//create totals for the hours
-var tableHead = document.getElementById('tableHead');
-var hoursTotal = document.createElement('th');
-hoursTotal.textContent = 'Total';
-tableHead.appendChild(hoursTotal);
-
-for (var k = 0; k < hours.length; k++) {
-  var hourlyTotals = pike.hourlySales[k] + seaTac.hourlySales[k] + seattleCenter.hourlySales[k] + capitolHill.hourlySales[k] + alki.hourlySales[k];
-  hourlyTotalsArray.push(hourlyTotals);
-}
-
-
-for (var l = 0; l < hours.length; l++) {
-  var trTotalEl = document.getElementById(hours[l]);
-  var totalTd = document.createElement('td');
-  totalTd.textContent = hourlyTotalsArray[l];
-  trTotalEl.appendChild(totalTd);
-}
-
-//creating totaltotal
-
-var totalTotal = 0;
-for (var m = 0; m < hourlyTotalsArray.length; m++) {
-  totalTotal += hourlyTotalsArray[m];
-}
-
-var totalTotalEl = document.createElement('td');
-totalTotalEl.textContent = totalTotal;
-tfooterEl.appendChild(totalTotalEl);
-
-
-
-
+makeTotal();
 
 var newStore = document.getElementById('newStore');
 
@@ -133,7 +134,20 @@ function newStoreHandler(event) {
   var idTag = storeName;
   console.log(storeName, minCust, maxCust, cookiesAvg);
 
+  //remove hours total
+  var tableHeadEl = document.getElementById('tableHead');
+  tableHeadEl.removeChild(tableHeadEl.lastChild);
+  var totalTotalEl = document.getElementById('totalTotal');
+  tfooterEl.removeChild(totalTotalEl);
+
+  for (var n = 0; n < hours.length; n++) {
+    var removeData = document.getElementById(hours[n]);
+    removeData.removeChild(removeData.lastChild);
+  }
+
   new Stand(idTag, storeName, minCust, maxCust, cookiesAvg);
+
+  makeTotal();
 
   event.target.storeName.value = null;
   event.target.minCust.value = null;
@@ -142,7 +156,3 @@ function newStoreHandler(event) {
 }
 
 newStore.addEventListener('submit', newStoreHandler, false);
-
-
-
-
